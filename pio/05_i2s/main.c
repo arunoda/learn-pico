a5d0f70af848c3f6fc8ac349f2d0281b85003ff1
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-04-02 20:18:40 +0530

    Add a basic i2s setup

---
+++ pio/05_i2s/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "i2s.pio.h"
+
+#define I2S_BCK_PIN 0
+#define I2S_WS_PIN 1
+#define I2S_DATA_PIN 2
+
+#define SAMPLE_RATE 24000
+
+uint32_t get_desired_clock_khz(uint32_t sample_rate) {
+    if (sample_rate %  8000 == 0) {
+        return 144000;
+    }
+
+    if (sample_rate % 11025 == 0) {
+        return 135600;
+    }
+
+    return 150000;
+}
+
+int main()
+{
+    stdio_init_all();
+
+    // PIO initialization
+    PIO pio = pio0;
+    uint sm = 0;
+    uint offset;
+
+    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&i2s_program, &pio, &sm, &offset, I2S_BCK_PIN, 3, true);
+    hard_assert(success);
+
+    pio_sm_config c = i2s_program_get_default_config(offset);
+
+    //pin config
+    sm_config_set_out_pins(&c, I2S_DATA_PIN, 1);
+    sm_config_set_out_shift(&c, true, true, 32);
+    sm_config_set_sideset_pins(&c, I2S_BCK_PIN);
+
+    // Increase the FIFO size to 8 words
+    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_TX);
+
+    //gpio setup
+    pio_gpio_init(pio, I2S_BCK_PIN);
+    pio_gpio_init(pio, I2S_WS_PIN);
+    pio_gpio_init(pio, I2S_DATA_PIN);
+    pio_sm_set_consecutive_pindirs(pio, sm, I2S_BCK_PIN, 3, true);
+
+    //clock setup
+    set_sys_clock_khz(get_desired_clock_khz(SAMPLE_RATE), true);
+    float div = clock_get_hz(clk_sys) / (SAMPLE_RATE * 32 * i2s_BCK_CYCLES);
+    sm_config_set_clkdiv(&c, div);
+
+    //start the sm
+    pio_sm_init(pio, sm, offset, &c);
+    pio_sm_set_enabled(pio, sm, true);
+
+    while (true) {
+        pio_sm_put_blocking(pio, sm, 0x00030001);
+    }
+}
