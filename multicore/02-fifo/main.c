107da602dd92ca5c8b9076879351c38a3b332dcf
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-04-09 06:22:01 +0530

    Add fifo example

---
+++ multicore/02-fifo/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+
+void main2() {
+    while (true)
+    {
+        uint32_t val = multicore_fifo_pop_blocking();
+        printf("Core2: %d\n", val);
+        val += 1;
+        sleep_ms(1000);
+        multicore_fifo_push_blocking(val);
+    }
+} 
+
+int main()
+{
+    stdio_init_all();
+
+    multicore_launch_core1(main2);
+    multicore_fifo_push_blocking(1);
+
+    while (true) {
+        uint32_t val = multicore_fifo_pop_blocking();
+        printf("Core1: %d\n", val);
+        val += 1;
+        sleep_ms(1000);
+        multicore_fifo_push_blocking(val);
+    }
+}
