6434d282d93ae72c73c6ca7c85ac8fb674d1ff04
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-07-08 17:53:47 +0530

    Using the master mode but still no luck

--- 8388/04_i2s/main.cpp
+++ 8388/04_i2s/main.cpp
@@ -57,45 +57,34 @@ uint32_t get_desired_clock_khz(uint32_t sample_rate) {
 }
 
 void init_pio_i2s() {
-    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&i2s_program, &pio, &sm, &offset, I2S_BCK_PIN, 4, true);
+    bool success = pio_claim_free_sm_and_add_program_for_gpio_range(&i2s_slave_program, &pio, &sm, &offset, I2S_DATA_PIN, 1, true);
     hard_assert(success);
 
-    pio_sm_config c = i2s_program_get_default_config(offset);
+    pio_sm_config c = i2s_slave_program_get_default_config(offset);
 
     //pin out config
     sm_config_set_out_pins(&c, I2S_DATA_PIN, 1);
-    // add support LSBJ with autopull after 32 bits (simply because our DAC is PT8211)
-    // It's a 16bit stereo DAC with LSBJ format
+    // Enable autopull after 32 bits (16 bits left + 16 bits right) for 16-bit LSB format
     sm_config_set_out_shift(&c, false, true, 32);
-    sm_config_set_sideset_pins(&c, I2S_BCK_PIN);
-
-    // // //pin in config
-    sm_config_set_in_pins(&c, I2S_DATA_IN_PIN);
-    sm_config_set_in_pin_count(&c, 1);
-    sm_config_set_in_shift(&c, false, true, 32);
-
-    // // Increase the FIFO size to 8 words
-    // sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_TX);
-
-    //gpio setup
-    pio_gpio_init(pio, I2S_BCK_PIN);
-    pio_gpio_init(pio, I2S_WS_PIN);
-    pio_gpio_init(pio, I2S_DATA_PIN);
-    pio_gpio_init(pio, I2S_DATA_IN_PIN);
     
-    // Set output pins as outputs (BCK, WS, DATA_OUT)
-    pio_sm_set_consecutive_pindirs(pio, sm, I2S_BCK_PIN, 3, true);
+    // No side-set pins needed - we're following external clocks
+    // No in pins needed for this output-only example
+
+    //gpio setup - set up all pins that PIO needs to access
+    pio_gpio_init(pio, I2S_BCK_PIN);    // PIO needs to read BCK for wait instructions
+    pio_gpio_init(pio, I2S_WS_PIN);     // PIO needs to read WS for wait instructions  
+    pio_gpio_init(pio, I2S_DATA_PIN);   // PIO needs to output data
     
-    // Set input pin as input (DATA_IN)
-    pio_sm_set_consecutive_pindirs(pio, sm, I2S_DATA_IN_PIN, 1, false);
+    // Set BCK and WS pins as inputs (they come from the codec)
+    pio_sm_set_consecutive_pindirs(pio, sm, I2S_BCK_PIN, 1, false);  // BCK input
+    pio_sm_set_consecutive_pindirs(pio, sm, I2S_WS_PIN, 1, false);   // WS input
+    
+    // Set data pin as output
+    pio_sm_set_consecutive_pindirs(pio, sm, I2S_DATA_PIN, 1, true);  // DATA output
 
-    //clock setup
-    set_sys_clock_khz(get_desired_clock_khz(SAMPLE_RATE), true);
-    // here we run this as twice the speed to support reading data from i2s as well
-    // in the pio code, we use nop to match the clocks
-    float div = clock_get_hz(clk_sys) / (SAMPLE_RATE * 32 * i2s_BCK_CYCLES * 2);
-    sm_config_set_clkdiv(&c, div);
-
+    //clock setup - no divider needed since we follow external clocks
+    // The PIO will run at full speed and wait for external clock edges
+    
     //start the sm
     pio_sm_init(pio, sm, offset, &c);
     pio_sm_set_enabled(pio, sm, true);
@@ -153,23 +142,31 @@ void setup_8388() {
     sleep_ms(10);                      // give codec time to power up
 
     // Power up ES8388 codec
-    es8388_write(0, 0b00000110); // R0: set to defaults: 0000 0110
+    es8388_write(0, 0b00010110); // R0: set to defaults: 0000 0110
     es8388_write(1, 0b01010000); // R1: ebable analog power: 0101 0000
     es8388_write(2, 0b00000000); // R2: enable chip power: 0000 0000
-    es8388_write(3, 0b00001100); // R3: enable ADCs: 0000 1100
+    es8388_write(3, 0b00001000); // R3: enable ADCs: 0000 1100
     es8388_write(4, 0b00111100); // R4: enable DACs: 0011 1100
     es8388_write(5, 0b00000000); // R5: no low power mode: 0000 0000
     es8388_write(6, 0b00000000); // R6: no low power mode: 0000
     es8388_write(7, 0b01111100); // R7: anaog voltage mgt (default): 0111 1100
-    es8388_write(8, 0b00000000); // R8: master/slave mode: slave mode selected: 0000 0000
+    es8388_write(8, 0b10000000); // R8: master/slave mode: master with MCLK/4
 
     // ADC settings (we will do more later, no needed for this example)
     es8388_write(9, 0b00000000); // R9: mic gain to 0db: 0000 0000
+    es8388_write(10, 0b00000000); // Selet inputs
+    es8388_write(11, 0b00000010); // Select ADC inputs
+    es8388_write(12, 0b00001101); // R12: Set ADC to 16bit, LSBJ format, and enable ADC clock
+    es8388_write(13, 0b00000010); // R13: ADC set clocks
     es8388_write(14, 0b00110000);
     es8388_write(15, 0b00100000);
     es8388_write(16, 0b00000000); // R16: ADCL volume, we need to set it to 0000 0000 of 0db
     es8388_write(17, 0b00000000); // R17: DACR volume, we need to set it to 0000 0000 of 0db
-    
+    es8388_write(18, 0b00111000);
+    es8388_write(19, 0b10110000);
+    es8388_write(20, 0b00110011);
+    es8388_write(21, 0b00000110);
+
     // DAC and output settings
     es8388_write(23, 0b00011010); // R23: 16bit with LSB
     es8388_write(24, 0b00000010); // R24: May not need since we use slave mode: but set to 256 MCLK/Sample Rate
@@ -180,6 +177,7 @@ void setup_8388() {
     es8388_write(38, 0b00000000); // R38: LIN select: 0000 0000 (LIN1 -> LEFT, RIN1 -> RIGHT)
     es8388_write(39, 0b10010000); // R39: need to enable DAC to Mixer and 0db volume 0101 0000
     es8388_write(42, 0b10010000); // R42: need to enable RIN to Mixer and 0db volume 0101 0000
+    es8388_write(43, 0b11000000);
     es8388_write(45, 0b00000000);
     es8388_write(46, 0b00011110); // R46: LOUT1 volume: need to change to 00011110 for 0db
     es8388_write(47, 0b00000000); // R47: ROUT1 volume: moved to 0000 0000 for -45db
@@ -192,9 +190,6 @@ int main()
 {
     stdio_init_all();
     setup_8388();
-
-    init_pio_i2s();
-    init_pwm_mclk();  // Use PWM instead of PIO for MCLK
 
     // Generate sine wave samples for stereo output
     const int SAMPLES = SAMPLE_RATE / 110; // One period of 440Hz tone (A4 note)
@@ -215,10 +210,14 @@ int main()
 
     int32_t *buffer = (int32_t *)sine_wave;
     int32_t read_sample = 0;
+
+    init_pio_i2s();       // Initialize I2S slave PIO
+    init_pwm_mclk();      // Use PWM for MCLK to codec
+
     while (true) {
+        // setup_8388();
         for (int i = 0; i < SAMPLES; i++) {
             pio_sm_put_blocking(pio, sm, buffer[i]);
-            read_sample = pio_sm_get_blocking(pio, sm); // Wait for the sample to be sent
         }
     }
 }
